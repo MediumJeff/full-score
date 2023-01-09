0d297dbcf28303d0616879003e40430d7459e8ef
Jeff McCracken <jjmccrac@gmail.com>
2023-01-09 12:03:04 -0800

    Expanding student schema

--- backend/models/studentModel.js
+++ backend/models/studentModel.js
@@ -1,20 +1,67 @@
 const mongoose = require('mongoose');
 
-const studentSchema = mongoose.Schema({
-    name: {
+const parentSchema = mongooseSchema({
+    firstName: {
         type: String,
-        required: [true, 'Please enter your name.']
+        required: [true, 'Please enter your first name.']
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Please enter your last name.']
+    },
+})
+
+const studentSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, 'Please enter your first name.']
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Please enter your last name.']
     },
     studentId: {
         type: Number,
         required: [true, 'Please enter your Student ID number.'],
         unique: [true, 'This student number is already in use.']
     },
+    gradeLevel: {
+        type: String,
+        required: [true, 'Please enter your current grade level.']
+    },
     instrument: {
         type: String,
         default: [],
         required: [true, 'Please enter your primary instrument or vocal range.']
-    }
+    },
+    ensembles: {
+        type: String,
+        default: [],
+        required: [true, 'Please enter at least one ensemble membership.']
+    },
+    email: {
+        type: String,
+        unique: [true, "Email already in use."],
+        lowercase: true,
+        trim: true,
+        required: [true, "Please enter a valid email."],
+        validate: {
+            validator: function (v) {
+                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+            },
+            message: '{VALUE} is not a valid email!'
+        }
+    },
+    phone: {
+        type: String,
+        trim: true,
+        validate: {
+            validator: function (v) {
+                return /^[0-9]{10}/.test(v);
+            },
+            message: '{VALUE} is not a valid phone number!'
+        }
+    },
 }, {
     timestamps: true
 })
