573157455e14bb53d7d3cc7264a063d31d3d6298
Jeff McCracken <jjmccrac@gmail.com>
2023-02-20 11:30:34 -0800

    Login, out, and registration functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,10 @@
 import { useState, useEffect } from "react";
 import { FaSignInAlt } from "react-icons/fa";
-
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { login, reset } from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -9,6 +13,25 @@ function Login() {
   })
 
   const { email, password } = formData
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+
+    if (isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -19,37 +42,48 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault()
+
+    const userData = {
+      email,
+      password
+    }
+
+    dispatch(login(userData))
+  }
+
+  if(isLoading) {
+    return <Spinner />
   }
 
   return (
     <>
       <section className="heading">
         <h1>
-          <FaSignInAlt />Login
+          <FaSignInAlt /> Login
         </h1>
         <p>Login to your account</p>
       </section>
       <section className="form">
         <form onSubmit={onSubmit}>
           <div className="form-group">
-            <input 
-              type="text" 
-              className="form-control" 
-              id="email" 
-              name="email" 
-              value={email} 
-              placeholder="Enter your email" 
-              onChange={onChange} 
+            <input
+              type="text"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
             />
           </div><div className="form-group">
-            <input 
-              type="password" 
-              className="form-control" 
-              id="password" 
-              name="password" 
-              value={password} 
-              placeholder="Choose a password" 
-              onChange={onChange} 
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Choose a password"
+              onChange={onChange}
             />
           </div>
           <div className="form-group">
