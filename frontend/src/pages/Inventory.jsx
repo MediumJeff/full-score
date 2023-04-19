324d0b2e3313ec3f32c807db1df9e58d82cf0b1c
Jeff McCracken <jjmccrac@gmail.com>
2023-04-19 11:53:48 -0700

    Display inventory table

--- frontend/src/pages/Inventory.jsx
+++ frontend/src/pages/Inventory.jsx
@@ -2,6 +2,7 @@ import { FaClipboardList } from 'react-icons/fa';
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from "react-router-dom";
+import { Table } from "react-bootstrap" ;
 import { toast } from 'react-toastify';
 import { getInstrument, deleteInstrument, updateInstrument, reset } from '../features/inventory/inventorySlice';
 
@@ -26,7 +27,8 @@ const Inventory = () => {
     return () => {
         dispatch(reset())
     }
-}, [ user, isError, isSuccess, message, dispatch, navigate])
+}, [user, isError, isSuccess, message, dispatch, navigate])
+
 
   return (
     <>
@@ -36,7 +38,28 @@ const Inventory = () => {
         </h1>
       </div>
       <div className='inventoryTable'>
-        <p>{instruments.type}</p>
+        <Table>
+          <thead>
+            <tr>
+              <th>Type</th>
+              <th>Make</th>
+              <th>Model</th>
+              <th>Serial Number</th>
+              <th>Notes</th>
+            </tr>
+          </thead>
+          <tbody>
+            {instruments.map((inst, id) => (
+              <tr data-index={id}>
+                <td>{inst.type}</td>
+                <td>{inst.make}</td>
+                <td>{inst.model}</td>
+                <td>{inst.serialNumber}</td>
+                <td>{inst.damageNotes}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </div>
     </>
   )
