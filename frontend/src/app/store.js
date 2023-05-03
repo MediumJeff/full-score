b8f894fd17eb96670f3f8634b73815eeafed6446
Jeff McCracken <jjmccrac@gmail.com>
2023-05-03 13:24:10 -0700

    Basic Student UI

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import calendarReducer from '../features/calendar/calendarSlice';
 import inventoryReducer from '../features/inventory/inventorySlice';
+import studentReducer from '../features/students/studentSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     events: calendarReducer,
     instruments: inventoryReducer,
+    students: studentReducer,
   },
 });
--- frontend/src/pages/Students.jsx
+++ frontend/src/pages/Students.jsx
@@ -6,8 +6,32 @@ import { Table, Button, Form } from "react-bootstrap";
 import Modal from "react-bootstrap/Modal";
 import { toast } from 'react-toastify';
 import { FaSchool } from 'react-icons/fa';
+import { getStudent, reset } from '../features/students/studentSlice';
 
 const Students = () => {
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  // Load information for instrument inventory
+  const { user, isError, isSuccess, message } = useSelector((state) => state.auth)
+  const { students } = useSelector((state) => state.students)
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+    if (isSuccess) {
+      navigate('/students')
+    }
+    dispatch(getStudent())
+
+    return () => {
+      dispatch(reset())
+    }
+  }, [user, isError, isSuccess, message, dispatch, navigate])
+
+
   return (
     <>
       <div>
@@ -15,8 +39,30 @@ const Students = () => {
           <FaSchool /> Students
         </h1>
       </div>
-      <div>
-
+      <div className='inventoryTable mt-5'>
+        <h2>Roster</h2>
+        <Table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Student ID</th>
+              <th>Grade Level</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {students.map((stnt, id) => (
+              <tr key={id}>
+                <td>{stnt.firstName}</td>
+                <td>{stnt.lastName}</td>
+                <td>{stnt.studentId}</td>
+                <td>{stnt.gradeLevel}</td>
+                <td><Button variant="success" active className='btn'>Inspect</Button></td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </div>
     </>
   )
